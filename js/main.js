10c5a0f0649e3e7eabbf006bcd8b26fa762bbf30
RafayKz <kzr7091@gmail.com>
2025-07-03 03:10:12 +0500

    set popop

--- js/main.js
+++ js/main.js
@@ -161,3 +161,7 @@ function toggleAnswer(answerId) {
         currentAnswer.style.display = 'block';
     }
 }
+
+ function closePopup() {
+    document.getElementById('popupOverlay').style.display = 'none';
+  }
